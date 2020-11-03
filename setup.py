af6abb4491c9c9ce545c36fe4ba318e6cadad43e
Etienne de Montalivet <demontalivete@fr.ray.group>
2020-11-03 14:38:47 +0100

    update version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
-    version='0.1.0',  # Required
+    version='0.1.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
