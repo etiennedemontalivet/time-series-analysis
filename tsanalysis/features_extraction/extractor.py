00d28303a89b3d2d5d3f2e1d2e7948c8af50a50c
Etienne de Montalivet <demontalivete@fr.ray.group>
2021-02-15 13:08:52 +0100

    FIx imports

--- tsanalysis/features_extraction/extractor.py
+++ tsanalysis/features_extraction/extractor.py
@@ -8,10 +8,10 @@ from typing import Callable
 from typing import List
 import pandas as pd
 
-from tsanalysis.features_extraction import extract_cepd_features
-from tsanalysis.features_extraction import extract_td_features
-from tsanalysis.features_extraction import extract_wd_features
-from tsanalysis.features_extraction import extract_fd_features
+from features_extraction import extract_cepd_features
+from features_extraction import extract_td_features
+from features_extraction import extract_wd_features
+from features_extraction import extract_fd_features
 
 # pylint: disable=too-many-arguments,dangerous-default-value
 def extract_all_features(
